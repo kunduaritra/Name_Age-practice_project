6cdf062f9940be451d2a9f3a3786c1717fc0e472
kunduaritra <kunduaritra7@gmail.com>
2023-07-10 22:18:46 +0530

    validation of invalid input without overlay added

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -8,7 +8,15 @@ const AddUser = (props) => {
     const [enteredAge, setEnteredAge] = useState('')
     const addUserHandler = (event) => {
         event.preventDefault();
+        if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 ){
+            return;
+        }
+        if( +enteredAge < 1 ) {
+            return;
+        }
         console.log(enteredUsername ,enteredAge)
+        setEnteredUsername('');
+        setEnteredAge('');
     }
     const usernameChangeHandler = event => {
         setEnteredUsername(event.target.value);
